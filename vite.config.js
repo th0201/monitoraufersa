af3b619b949676b12045bb18860c0751641c2aa0
Thayna <thayna.lima@alunos.ufersa.edu.br>
2026-06-17 15:29:05 -0300

    Adicionando PWA

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ],
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'MonitoraUFERSA',
+        short_name: 'MonitoraUFERSA',
+        description: 'Sistema de agendamento de monitorias',
+        theme_color: '#10b981',
+        background_color: '#020617',
+        display: 'standalone',
+        start_url: '/',
+
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
